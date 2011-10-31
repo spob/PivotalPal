2f2d3c3b0dd1982361874075fd95729650de996f
Bob Sturim <bob@sturim.org>
2011-10-30 20:59:02 -0400

    fixed database migration

--- db/migrate/20111008014223_add_tenant_fk_to_project.rb
+++ db/migrate/20111008014223_add_tenant_fk_to_project.rb
@@ -1,11 +1,9 @@
 class AddTenantFkToProject < ActiveRecord::Migration
   def self.up
     add_column :projects, :pivotal_identifier, :integer, :null => false
-    add_index :projects, [:tenant_id, :pivotal_identifier], :unique => true
   end
 
   def self.down
-    remove_index :projects, [:tenant_id, :pivotal_identifier]
     remove_column :projects, :pivotal_identifier
   end
 end
